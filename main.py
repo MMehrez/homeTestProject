f6f9ef0c007c5c17e1083f62bc1cd0798d467923
Mohamed W. Mehrez <m.mehrez.said@mun.ca>
2018-04-06 17:54:38 +0200

    style changes and some comments added

--- main.py
+++ main.py
@@ -3,12 +3,16 @@
 Created on Tue Apr 03 19:09:37 2018
 @author: Mohamed W. Mehrez Said
 email: m.mehrez.said@mun.ca
+Description: This program is a simple network service that returns latitude and longitude coordinates 
+of a give address or a list of addresses.
+Run this program using python version 3.0 or newer versions.
 """
 import urllib.request, urllib.parse
 import json, time
 
-def geocodingGoogle(index,url):
-    params = {'address': requested_addresses[index]}
+# google geocoding function
+def geocodingGoogle(requested_address,url):
+    params = {'address': requested_address}
     req_url = url + urllib.parse.urlencode(params)
     try:
         response = urllib.request.urlopen(req_url)
@@ -19,8 +23,9 @@ def geocodingGoogle(index,url):
         location = 'Error'
     return location
 
-def geocodingHere(index,url):
-    params = {'searchtext': requested_addresses[index],'app_id':'ZuuaESOoZ3r0TfUpSNLN',\
+# google geocoding function
+def geocodingHere(requested_address,url):
+    params = {'searchtext': requested_address,'app_id':'ZuuaESOoZ3r0TfUpSNLN',\
               'app_code':'cNeC1goF-_A2H5Zbth4vmQ'}
     req_url = url + urllib.parse.urlencode(params)
     try:
@@ -38,7 +43,6 @@ if __name__ == '__main__':
     # welcome and information message
     print ("Welcome to HOME-TEST Geocoding Service!")
     print ("This service returns the latitude and longitude coordinates for (a) given address(es).")       
- 
     time.sleep(1)
     input("Press Enter to continue...")
     
@@ -46,14 +50,14 @@ if __name__ == '__main__':
     requested_addresses = input('Enter the Address(es) separated by a (;)')
     requested_addresses = requested_addresses.split(";")
 
-    url_google = 'https://maps.googleapis.com/maps/api/geocode/json?'
-    url_here = 'https://geocoder.cit.api.here.com/6.2/geocode.json?'
+    url_google = 'https://maps.googleapis.com/maps/api/geocode/json?' #google main url for geocoding requests
+    url_here = 'https://geocoder.cit.api.here.com/6.2/geocode.json?'  #Here main url for geocoding requests
 
     i = 0;
     while i < len(requested_addresses):
-        location = geocodingGoogle(i,url_google)
+        location = geocodingGoogle(requested_addresses[i],url_google) #primary geocoding service
         if location == 'Error':
-            location = geocodingHere(i,url_here)
+            location = geocodingHere(requested_addresses[i],url_here) #Secondary geocoding service
         print ("The coordinates of",requested_addresses[i],"are: (latitude:",\
                location['lat'],")","and (longitude:",location['lng'],")")
         i+=1
