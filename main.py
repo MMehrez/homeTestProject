0d44cb27e7c4f099941254e287117e3fafcf2e5c
Mohamed W. Mehrez <m.mehrez.said@mun.ca>
2018-04-06 21:02:46 +0200

    documentation started

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ import urllib.request, urllib.parse
 import json, time
 
 # google geocoding function
-def geocodingGoogle(requested_address,url):
+def geocodingGoogle(requested_address):
+    url_google = 'https://maps.googleapis.com/maps/api/geocode/json?' #google main url for geocoding requests
     params = {'address': requested_address}
-    req_url = url + urllib.parse.urlencode(params)
+    req_url = url_google + urllib.parse.urlencode(params)
     try:
         response = urllib.request.urlopen(req_url)
         data = response.read().decode()
@@ -24,10 +25,11 @@ def geocodingGoogle(requested_address,url):
     return location
 
 # google geocoding function
-def geocodingHere(requested_address,url):
+def geocodingHere(requested_address):
+    url_here = 'https://geocoder.cit.api.here.com/6.2/geocode.json?'  #Here main url for geocoding requests
     params = {'searchtext': requested_address,'app_id':'ZuuaESOoZ3r0TfUpSNLN',\
               'app_code':'cNeC1goF-_A2H5Zbth4vmQ'}
-    req_url = url + urllib.parse.urlencode(params)
+    req_url = url_here + urllib.parse.urlencode(params)
     try:
         response = urllib.request.urlopen(req_url)
         data = response.read().decode()
@@ -49,15 +51,12 @@ if __name__ == '__main__':
     # address request message
     requested_addresses = input('Enter the Address(es) separated by a (;)')
     requested_addresses = requested_addresses.split(";")
-
-    url_google = 'https://maps.googleapis.com/maps/api/geocode/json?' #google main url for geocoding requests
-    url_here = 'https://geocoder.cit.api.here.com/6.2/geocode.json?'  #Here main url for geocoding requests
-
+      
     i = 0;
     while i < len(requested_addresses):
-        location = geocodingGoogle(requested_addresses[i],url_google) #primary geocoding service
+        location = geocodingGoogle(requested_addresses[i])#primary geocoding service (google)
         if location == 'Error':
-            location = geocodingHere(requested_addresses[i],url_here) #Secondary geocoding service
+            location = geocodingHere(requested_addresses[i]) #Secondary geocoding service (Here)
         print ("The coordinates of",requested_addresses[i],"are: (latitude:",\
                location['lat'],")","and (longitude:",location['lng'],")")
         i+=1
